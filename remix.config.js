486281f7dd8abaa35cf3cb39551fbe7fec374f85
Logan McAnsh <logan@mcan.sh>
2023-02-16 19:30:50 -0500

    remove patch, force remix-dev to be 1.13
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- remix.config.js
+++ remix.config.js
@@ -1,12 +1,10 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  publicPath: "/build/",
   ignoredRouteFiles: ["**/.*"],
-  // When running locally in development mode, we use the built-in remix
-  // server. This does not understand the vercel lambda module format,
-  // so we default back to the standard build output.
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
-  serverBuildPath: "api/index.js",
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
+  future: {
+    v2_routeConvention: true,
+  },
 };
